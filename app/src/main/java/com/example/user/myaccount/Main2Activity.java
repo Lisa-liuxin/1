66cf6bc7adc727895d6cc8deaf2a555e13a28189
wangjie <420733721@qq.com>
2018-04-08 22:49:06 +0800

    4yue8ri

--- app/src/main/java/com/example/user/myaccount/Main2Activity.java
+++ app/src/main/java/com/example/user/myaccount/Main2Activity.java
@@ -14,7 +14,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
-
+    private EditText et_leibie;
+    private EditText et_username;
+    private EditText et_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,64 +30,90 @@ public class Main2Activity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
-    };
 
-       public class TextWatcherDemo extends Activity {
-
-        private EditText text;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            // TODO Auto-generated method stub
-            super.onCreate(savedInstanceState);
-            //获得布局文件
-            setContentView(R.layout.activity_main2);
-            //获取输入框
-            text = (EditText) this.findViewById(R.id.et_leibie);
-
-            text.addTextChangedListener(new TextWatcher() {
-                //输入框监听
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before,
-                                          int count) {
-                    //Log.i("Text", "onTextChanged:"+s+"<><>"+start+"<><>
+        //获取输入框
+        et_leibie = (EditText) this.findViewById(R.id.et_leibie);
+        et_leibie.addTextChangedListener(new TextWatcher() {
+            //输入框监听
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                //Log.i("Text", "onTextChanged:"+s+"<><>"+start+"<><>
 
 
-                }
+            }
 
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count,
-                                              int after) {
-                    //Log.i("Text", "beforeTextChanged:"+s+"<><>"+start+"<><>"+count+"<><>"+after);
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                //Log.i("Text", "beforeTextChanged:"+s+"<><>"+start+"<><>"+count+"<><>"+after);
 
-                }
+            }
 
-                @Override
-                public void afterTextChanged(Editable s) {
-                    //Log.i("Text", "afterTextChanged:"+s);
+            @Override
+            public void afterTextChanged(Editable s) {
+                //Log.i("Text", "afterTextChanged:"+s);
 
-                }
-            });
-        }
+            }
+        });
+        //获取输入框
+        et_username = (EditText) this.findViewById(R.id.et_username);
+        et_username.addTextChangedListener(new TextWatcher() {
+            //输入框监听
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                //Log.i("Text", "onTextChanged:"+s+"<><>"+start+"<><>
 
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                //Log.i("Text", "beforeTextChanged:"+s+"<><>"+start+"<><>"+count+"<><>"+after);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //Log.i("Text", "afterTextChanged:"+s);
+
+            }
+        });
+        //获取输入框
+        et_password = (EditText) this.findViewById(R.id.et_password);
+        et_password.addTextChangedListener(new TextWatcher() {
+            //输入框监听
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                //Log.i("Text", "onTextChanged:"+s+"<><>"+start+"<><>
+
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                //Log.i("Text", "beforeTextChanged:"+s+"<><>"+start+"<><>"+count+"<><>"+after);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //Log.i("Text", "afterTextChanged:"+s);
+
+            }
+        });
     }
-    public class MainActivity extends Activity {
 
-        private EditText et_leibie;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            et_leibie=(EditText)this.findViewById(R.id.et_leibie);
-        }
-
-        // 按钮的监听器方法
-        public void show(View view) {
-            switch (view.getId()) {
-                case R.id.et_save:// 保存
+    public void show(View view) {
+        switch (view.getId()) {
+            case R.id.et_save:// 保存
             /*
              * 1.创建SharedPerferences的对象 , 参数一:保存数据的文件名字 文件名如果存在 就不会创建 否则就会创建
              * 参数二mode;数据的操作模式 如下:
@@ -97,35 +125,37 @@ public class Main2Activity extends AppCompatActivity {
              * MODE_WORLD_READABLE：表示当前文件可以被其他应用读取.
              * MODE_WORLD_WRITEABLE：表示当前文件可以被其他应用写入
              */
-                    SharedPreferences sharedPreferences = this.getSharedPreferences(
-                            "SharedPreferences", this.MODE_PRIVATE);
-                    // 2,获得sharedPreferences的编辑对象
-                    SharedPreferences.Editor editer = sharedPreferences.edit();
-                    // 3,向文件中put数据
-                    String str = et_leibie.getText().toString().trim();
-                    //假设不为空
-                    editer.putString("name", str);
-                    // 4.使用commit提交数据
-                    editer.commit();
-                    // 提示信息
-                    Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
-                    break;
+                SharedPreferences sharedPreferences = this.getSharedPreferences(
+                        "SharedPreferences", this.MODE_PRIVATE);
+                // 2,获得sharedPreferences的编辑对象
+                SharedPreferences.Editor editer = sharedPreferences.edit();
+                // 3,向文件中put数据
+                String str1 = et_leibie.getText().toString().trim();
+                String str2 = et_username.getText().toString().trim();
+                String str3 = et_password.getText().toString().trim();
+                //假设不为空
+                editer.putString("leibie", str1);
+                editer.putString("username", str2);
+                editer.putString("password", str3);
+                // 4.使用commit提交数据
+                editer.commit();
+                // 提示信息
+                Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
+                break;
 
-                case R.id.et_read:
-                    // 读取刚刚保存的文件数据
-                    // 1,得到SharedPreferences的对象 参数一 参数二相似
-                    SharedPreferences sharedPreferencesRead = this
-                            .getSharedPreferences("SharedPreferences",
-                                    this.MODE_PRIVATE);
-                    // 2,根据保存的k键读取
-                    String name = sharedPreferencesRead.getString("name", "");
-                    // 输出信息
-                    et_leibie.setText(name);
-                    break;
-
-            }
+            case R.id.et_read:
+                // 读取刚刚保存的文件数据
+                // 1,得到SharedPreferences的对象 参数一 参数二相似
+                SharedPreferences sharedPreferencesRead = this
+                        .getSharedPreferences("SharedPreferences",
+                                this.MODE_PRIVATE);
+                // 2,根据保存的k键读取
+                String name = sharedPreferencesRead.getString("name", "");
+                // 输出信息
+                et_leibie.setText(name);
+                break;
 
         }
-    }
 
-};
+    }
+}
